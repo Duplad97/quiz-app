4acc2fc260f31df14d1b66285c663b4012af4f7e
Deák Dávid <davidd@themealplanner.com>
2020-07-20 14:38:10 +0200

    Updates

---
+++ precache-manifest.1f8aa513cec52f40de7714f484492b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa555313be010af76afff437b66b8df",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "00a4ebadea81113aaed6",
+    "url": "/quiz-app/static/css/main.89c5c9e6.chunk.css"
+  },
+  {
+    "revision": "012106f6aa17c4c7f2a6",
+    "url": "/quiz-app/static/js/2.37cce1b4.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/quiz-app/static/js/2.37cce1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a4ebadea81113aaed6",
+    "url": "/quiz-app/static/js/main.8355d72b.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
